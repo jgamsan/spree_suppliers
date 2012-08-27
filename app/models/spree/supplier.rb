c1102b495f4f43093ed67ca0d6fd4455a3c6cad3
jose miguel <miguel.gamazo@galiclick.com>
2012-08-27 18:48:57 +0200

    mas mejoras

--- app/models/spree/supplier.rb
+++ app/models/spree/supplier.rb
@@ -1,4 +1,6 @@
-class Spree::Supplier < ActiveRecord::Base
-  attr_accessible :description, :email, :facebook, :fax, :name, :notes, :phone, :title, :twitter, :website
-  has_many :products, :class_name => "Spree::Product"
+module Spree
+  class Supplier < ActiveRecord::Base
+    attr_accessible :description, :email, :facebook, :fax, :name, :notes, :phone, :title, :twitter, :website
+    has_many :products, :class_name => "Spree::Product"
+  end
 end
