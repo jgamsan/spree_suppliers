1277976772422564cdab92c414108d201fb63f14
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-12-06 10:45:37 +0100

    mas mejoras

--- app/overrides/add_suppliers_to_admin.rb
+++ app/overrides/add_suppliers_to_admin.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name => "add_suppliers_to_admin",
   :virtual_path => "spree/layouts/admin",
   :insert_bottom => "[data-hook='admin_tabs']",
-  :text => "<%= tab :suppliers, :label => 'Proveedores', :icon => 'icon-user-md' %>"
+  :text => "<%= tab :suppliers, :label => 'Proveedores', :icon => 'icon-barcode' %>"
 )
